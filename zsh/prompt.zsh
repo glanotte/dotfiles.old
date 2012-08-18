3bfced0ae9330bc270b793e443ed028550f22f8f
Geoff Lanotte <geofflanotte@gmail.com>
2012-08-18 10:05:09 -0400

    fixes the typing color

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -63,4 +63,4 @@ rvm_info() {
 }
 
 export PROMPT=$'$(git_status)%{\e[0;%(?.32.31)m%}⁕%{\e[0m%} '
-export RPROMPT=$'%{\e[0;90m%}$(project_pwd)${PR_YELLOW}$(rvm_info)$(git_prompt_info)${PR_WHITE}'
+export RPROMPT=$'%{\e[0;90m%}$(project_pwd)${PR_YELLOW}$(rvm_info)$(git_prompt_info)%{\e[0m%}'
